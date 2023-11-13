0d8e9339230a081e806e15eba9ffc08fcfa526ec
Pragatheesh IMAC <pragatheesh1995@gmail.com>
2023-11-13 23:08:51 +0530

    push check

---
+++ check/variables.ts
@@ -0,0 +1,11 @@
+let fName: string='Prgatheesh';
+let age: number=27;
+let average: number = 23.5;
+let married: boolean = false;
+let anonymous: any = 'Cricket'; //Here we can assign vallue of any date type
+
+console.log(`User Data ${fName} ${age} ${average} ${married} ${anonymous}`);
+
+anonymous = 95;
+
+console.log(`User Data ${fName} ${age} ${average} ${married} ${anonymous}`);
